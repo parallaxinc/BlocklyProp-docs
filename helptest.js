54d952b6a2d0f0c8c9b4a9f43cdae09cd46f039e
michel-cf <michel@creatingfuture.eu>
2016-01-05 20:05:11 +0100

    Rewrite links

--- helptest.js
+++ helptest.js
@@ -3,12 +3,54 @@
  */
 
 $(document).ready(function() {
-    $('#settings-form').on("submit", function() {
-        $.get($("#basepath").val(), function(textile) {
-            $("#pagecontent").html(convert(textile));
-        });
-
-
+    $("a.url-template").click(function() {
+        $("#basepath").val($(this).attr('href'));
         return false;
     });
+
+    $('#settings-form').on("submit", function() {
+        var file = $("#filepath").val();
+        if (file.length) {
+            location.hash = 'f=' + file;
+        }
+
+        $(window).hashchange();
+        return false;
+    });
+
+
+    $(window).hashchange(function(){
+        var hash = location.hash || '#f=index';
+        var hashFile =  hash.replace( /^#f=/, 'f=' );
+
+        if (hashFile.startsWith('f=')) {
+            var file = hash.replace(/^#f=/, '');
+
+            $("#filepath").val(file);
+
+            var basepath = $("#basepath").val();
+            if (!basepath.endsWith('/')) {
+                basepath += '/';
+            }
+
+            $.get(basepath + file + '.textile', function (textile) {
+                var html = convert(textile);
+          //      console.log(html);
+                html = rewrite(html);
+                $("#pagecontent").html(html);
+            });
+        }
+    });
+    $(window).hashchange();
 });
+
+function rewrite(htmlString) {
+    var html = $(htmlString);
+    $('a', html).each(function(index, element) {
+        var jElement = $(element);
+        if (jElement.attr('href').startsWith('help?f=')) {
+            jElement.attr('href', '#' + jElement.attr('href').substring(5));
+        }
+    });
+    return html;
+}
